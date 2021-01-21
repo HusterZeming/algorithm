8b7fd5383ed72b841619a5c3a84e822fa50e0957
Husterjin <1444865009@qq.com>
2021-01-21 21:49:56 +0800

    JZ03: 数组中重复的数字

---
+++ src/JZ03.java
@@ -0,0 +1,15 @@
+class JZ03 {
+    public int findRepeatNumber(int[] nums) {
+        int len = nums.length;
+        int i, j, index;
+        for(i = 0; i < len - 1; i++)
+        {
+            for(j = 0; j < i; j++)
+            {
+                if (nums[i] == nums[j])
+                    return nums[i];
+            }
+        }
+        return 0;
+    }
+}
